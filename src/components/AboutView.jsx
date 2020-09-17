3511bb1e8c15773eeb777225e44f64c3e708c813
mobiustrip <yvan@metatech.ie>
2020-09-17 11:16:09 +0200

    added about page

--- src/components/AboutView.jsx
+++ src/components/AboutView.jsx
@@ -1,13 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
+import { Container } from "semantic-ui-react";
+import { StyledCard } from "./common/styles";
 
-class AboutView extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return <h1>About</h1>;
-  }
-}
-
+const AboutView = () => (
+  <Container style={{ width: "60%" }}>
+    <StyledCard style={{ marginTop: "6em" }}>
+      <h1>Web Technologies Used</h1>
+      <p>
+        Tamska is a desktop web application for task management built with
+        modern web technologies.
+      </p>
+      <p>
+        The backend or server-side of the application is written in Python,
+        using the <a href="https://www.djangoproject.com/">Django framework</a>.
+        The backend acts as a REST API for the frontend or client-side of the
+        application. This was achieved using the{" "}
+        <a href="https://www.django-rest-framework.org/">
+          Django REST framework
+        </a>
+        .
+      </p>
+      <p>
+        The frontend of the application is written in JavaScript, using the{" "}
+        <a href="https://reactjs.org/">React</a> library. The CSS styles are a
+        mixture of custom styled-components and{" "}
+        <a href="https://react.semantic-ui.com/">Semantic-UI React</a>{" "}
+        components.
+      </p>
+    </StyledCard>
+  </Container>
+);
 export default AboutView;
