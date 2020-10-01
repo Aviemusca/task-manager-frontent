72dcb26071559fc8167bc4fb83524dc10baefb87
mobiustrip <yvan@metatech.ie>
2020-10-01 11:44:00 +0200

    added reload button to projects view - quick fix - still need to create userCredential context and refactor login/signup forms using hooks to fix underlying issue

--- src/components/projects/ProjectCardList.jsx
+++ src/components/projects/ProjectCardList.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { Button, Icon, Popup } from "semantic-ui-react";
 import styled from "styled-components";
 
 import ProjectCard from "./ProjectCard";
@@ -10,18 +11,35 @@ const StyledTitle = styled.h2`
   padding: 1em 1em;
   text-align: center;
 `;
-const ProjectCardList = ({ projects }) => (
-  <React.Fragment>
-    {projects.length !== 0 ? (
-      <CardGrid>
-        {projects.map((project) => (
-          <ProjectCard key={project.id} project={project} />
-        ))}
-      </CardGrid>
-    ) : (
-      <StyledTitle>You have no projects!</StyledTitle>
-    )}
-  </React.Fragment>
-);
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ProjectCardList = ({ projects }) => {
+  const handleClick = () => window.location.reload();
+
+  return (
+    <React.Fragment>
+      {projects.length !== 0 ? (
+        <CardGrid>
+          {projects.map((project) => (
+            <ProjectCard key={project.id} project={project} />
+          ))}
+        </CardGrid>
+      ) : (
+        <React.Fragment>
+          <StyledTitle>You have no projects!</StyledTitle>
+          <ButtonWrapper>
+            <Button onClick={handleClick} size="tiny" inverted>
+              Reload
+            </Button>
+          </ButtonWrapper>
+        </React.Fragment>
+      )}
+    </React.Fragment>
+  );
+};
 
 export default ProjectCardList;
